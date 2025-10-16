ab01b87ddeb20bb39fef62440f61e69bed5d9697
WelshBadger <136247787+WelshBadger@users.noreply.github.com>
2025-10-16 22:07:07 +0000

    Fix Netlify routing for dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/rally-detail/:id',
-        destination: '/rally-detail/[id]',
-        permanent: true
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
