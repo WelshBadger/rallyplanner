1506c1d2805778502c2c37ed7c2adbb8f53ecb25
WelshBadger <136247787+WelshBadger@users.noreply.github.com>
2025-10-21 21:57:02 +0000

    Restore working rally detail page with schedule handlers

--- pages/rally-detail/[id].js
+++ pages/rally-detail/[id].js
@@ -114,6 +114,7 @@ export default function RallyDetail() {
   }
 
 
+  const handleAddSchedule = () => {
     setEditingSchedule(null)
     setScheduleForm({ schedule_date: '', schedule_time: '', activity: '', location: '', notes: '' })
     setShowScheduleModal(true)
